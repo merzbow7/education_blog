64e30ed35aee67e7d3a2dd519753cdcd941c045b
sevenamber <sevenamber@gmail.com>
2021-06-25 04:06:04 +0300

    heroku release and fix issue

--- settings.py
+++ settings.py
@@ -14,6 +14,8 @@ class Configuration(object):
 
     # sqlalchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
